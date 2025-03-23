83179f76f09d4bb04da0f1e539acc94cd8c1fe60
xia-xx-cv <49708396+xia-xx-cv@users.noreply.github.com>
2025-03-23 20:41:13 +0800

    train_0205.py
    updated on 2025.03.23

--- tools/train_0205.py
+++ tools/train_0205.py
@@ -1,4 +1,5 @@
 import os, time, argparse
+# import sys
 import torch
 import torch.backends.cudnn as cudnn
 import torch.nn as nn
@@ -7,19 +8,27 @@ import torch.optim
 import torch.utils.data
 from torchvision.utils import save_image
 from datasets import data_reader_syn as data_reader
+# from util import transform_me as trans
 from util import transform as trans
 from util import config
 from util.saver import Saver
 from tensorboardX import SummaryWriter
-# from util.summaries import TensorboardSummary
-from util.util_smoke import AvgMeter, cal_cls_metrics, AuxTrain
+from util.util_smoke import AvgMeter, Metrics, AuxTrain
 from models.mynet_0205_ori import MyNet
 from util.cal_ssim import SSIM, MSE_bin
-# sys.path.append("../")
+import numpy as np
+import random
 
+
+def seed_all(seed):
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
 
 def get_parser():
-    parser = argparse.ArgumentParser(description='TA smoke density estimation')
+    parser = argparse.ArgumentParser(description='Dual smoke density estimation')
     parser.add_argument('--config', type=str, default='smoke_ori.yaml', help='config file')
     args = parser.parse_args()
     assert args.config is not None
@@ -30,6 +39,11 @@ def get_parser():
 def main():
     global args, writer, saver
     args = get_parser()
+
+    seed = args.seed
+    print(seed)
+    # seed_all(seed)
+    
     # cudnn.benchmark, cudnn.deterministic = True, True
     criterion = nn.MSELoss()
     # criterion2 = MSE_bin(th=0.2)
@@ -77,7 +91,7 @@ def main():
         trans.ToTensor(),
         trans.Normalize(mean=mean, std=std)
     ])
-    
+
     train_data = data_reader.Smoke(split='train', data_root=args.data_root,
                                    data_list=args.train_list, transform=tr_trans)
     train_loader = torch.utils.data.DataLoader(train_data, batch_size=args.batch_size, shuffle=True,
@@ -90,6 +104,8 @@ def main():
             trans.ToTensor(),
             trans.Normalize(mean=mean, std=std)
         ])
+        # val_data = data_reader.Smoke(split='val', data_root=args.data_root,
+        #                              data_list=args.val_list, transform=val_trans, synth=False)
         val_data = data_reader.Smoke(split='val', data_root=args.data_root,
                                      data_list=args.val_list, transform=val_trans)
         val_loader = torch.utils.data.DataLoader(val_data, batch_size=args.batch_size_val,
@@ -98,11 +114,11 @@ def main():
 
     loss_tr = 0.0
     for epoch in range(args.start_epoch, args.epochs):
-        epoch_log = epoch + 1
+        epoch_log = epoch + 1  # counting from 1
         f_loss_tr, mSSIM_tri, mIoU_tr = train(train_loader, model, optimizer, epoch, criterion, criterion2)
         loss_tr += f_loss_tr
-        writer.add_scalar('loss_train', f_loss_tr, epoch_log)
-        writer.add_scalar('mIoU_train', mIoU_tr, epoch_log)
+        writer.add_scalar('train/loss', f_loss_tr, epoch_log)
+        writer.add_scalar('train/mIoU', mIoU_tr, epoch_log)
         # writer.add_scalar('mAcc_train', mAuc_train, epoch_log)
 
         if epoch_log == 1:
@@ -116,34 +132,32 @@ def main():
             torch.save({'epoch': epoch_log, 'state_dict': model.state_dict(),
                         'optimizer': optimizer.state_dict()}, filename)
             print(loss_tr/epoch_log)
-            # if (epoch_log / args.save_freq > 2) and loss_train >= loss_tr*0.8/epoch_log:
-            #     deletename = saver.experiment_dir + '/train_epoch_' + str(epoch_log - args.save_freq * 2) + '.pth'
-            #     os.remove(deletename)
+
 
         if args.evaluate:
             loss_val, mIoU_val, mSSIM = validate(epoch_log, val_loader,
                                                  model, criterion, criterion2)
-            writer.add_scalar('loss_val', loss_val, epoch_log)
-            writer.add_scalar('mIoU_val', mIoU_val, epoch_log)
-            writer.add_scalar('mSSIM_val', mSSIM, epoch_log)
+            writer.add_scalar('val/loss', loss_val, epoch_log)
+            writer.add_scalar('val/mIoU', mIoU_val, epoch_log)
+            writer.add_scalar('val/mSSIM', mSSIM, epoch_log)
+            # writer.add_scalar('val/allAcc', allAcc_val, epoch_log)
 
 
 def train(train_loader, model, optimizer, epoch, criterion, criterion2):
     # kwargs = {'mode': 'bilinear', 'align_corners': True}
     batch_time, data_time = AvgMeter(), AvgMeter()
-    mytrain = AuxTrain(args.base_lr)
-    
     main_loss_meter, loss_meter = AvgMeter(), AvgMeter()
     l_tr, iou_meter, ss = 0.0, AvgMeter(), 0.0
     count = 0.0
 
     model.train()
-    tbar = tqdm(train_loader, position=0, leave=True)
+    # tbar = tqdm(train_loader, position=0, leave=True)
     num_img_tr = len(train_loader)
     max_iter = args.epochs * num_img_tr
     end = time.time()
-    for i, (inputs, target, _) in tbar(train_loader):
-    # for i, (inputs, target, _) in enumerate(train_loader):
+    metrics = Metrics()
+    # for i, (inputs, target) in tbar(train_loader):
+    for i, (inputs, target, _) in enumerate(train_loader):
         data_time.update(time.time() - end)
         if args.train_gpu:
             inputs = inputs.cuda(non_blocking=True)
@@ -151,12 +165,14 @@ def train(train_loader, model, optimizer, epoch, criterion, criterion2):
         optimizer.zero_grad()
 
         out3, aux = model(inputs, target)
+
         main_loss = criterion(out3[:, -1:], target[:, -1:]) + \
                     criterion(out3[:, 0:3:] * out3[:, -1:].data.repeat(1, 3, 1, 1),
                               target[:, 0:3:] * target[:, -1:].data.repeat(1, 3, 1, 1))
         aux_loss = criterion(aux, target[:, -1:]) + 1 - criterion2(out3[:, -1:], target[:, -1:])
-        
 
+
+        # criterion2
         loss = main_loss + aux_loss*args.aux_weight
         l_tr += loss.item()
         ss += criterion2(out3[:, -1:].data, target[:, -1:].data)
@@ -164,13 +180,19 @@ def train(train_loader, model, optimizer, epoch, criterion, criterion2):
         loss.backward()
         optimizer.step()
 
-        # ----compute and update metrics----
+        # ----compute metrics----
         n = inputs.size(0)
-        iou_meter.update(cal_cls_metrics(out3[:, -1:].data, target[:, -1:].data), n)
+        metrics.cal_cls_metrics(out3[:, -1:].data, target[:, -1:].data)
+        # ----update metrics----
+        iou_meter.update(metrics.iou, n)
         main_loss_meter.update(main_loss.item(), n), loss_meter.update(loss.item(), n)
+        # ----time----
+        batch_time.update(time.time() - end)
+        end = time.time()
 
         # ----learning rate decay----
         current_iter = epoch * num_img_tr + i + 1
+        mytrain = AuxTrain(args.base_lr)
         current_lr = mytrain.poly_lr(current_iter, max_iter, power=args.power)
         for index in range(0, args.index_split):
             optimizer.param_groups[index]['lr'] = current_lr
@@ -181,13 +203,16 @@ def train(train_loader, model, optimizer, epoch, criterion, criterion2):
             info = 'Epoch: [{}/{}][{}/{}]  ' \
                    'Data {data_time.val:.3f} ({data_time.avg:.3f}) '\
                    'Main {loss_meter.val:.4f} '\
-                   'IoU {iou_meter.val:.4f}'.format(epoch+1, args.epochs, i + 1, len(train_loader),
+                   'IoU {iou_meter.avg:.4f}'.format(epoch+1, args.epochs, i + 1, len(train_loader),
                                                     data_time=data_time,
                                                     loss_meter=loss_meter, iou_meter=iou_meter)
             print(info, 'ssim:{:.3f}'.format(ss/(i+1)))
         writer.add_scalar('train/loss_iter', loss.item(), i + num_img_tr * epoch)
         writer.add_scalar('train/iou_iter', iou_meter.avg, i + num_img_tr * epoch)
 
+    # ------------vis
+    save_image(out3[:, -1:].data.cpu(), saver.vis_dir + '/{}_alpha.png'.format(epoch))
+    save_image(target[:, -1:].cpu(), saver.vis_dir + '/{}_gt.png'.format(epoch))
     print('{}/{}'.format(count, num_img_tr))
     return loss_meter.avg, ss/num_img_tr, iou_meter.avg,
 
@@ -198,39 +223,42 @@ def validate(epoch, val_loader, model, criterion, criterion2):
     loss_meter, iou_meter, auc_meter = AvgMeter(), AvgMeter(), AvgMeter()
     ss = 0.0
     model.eval()
-    tbar = tqdm(val_loader, position=0, leave=True)
     num_img_tr = len(val_loader)
-    max_iter = args.epochs * num_img_tr
     end = time.time()
-    for i, (inputs, target, _) in tbar(val_loader):
-    # for i, (inputs, target, _) in enumerate(val_loader):
-        data_time.update(time.time() - end)
-        if args.train_gpu:
-            inputs = inputs.cuda(non_blocking=True)
-            target = target.cuda(non_blocking=True)
-        out = model(inputs)
-        out, target = out.data, target.data
-        loss = criterion(out[:, -1:], target)
-        ss += criterion2(out[:, -1:], target)
+    with torch.no_grad():
+        # tbar = tqdm(val_loader, position=0, leave=True)
+        # for i, (inputs, target, _) in tbar(val_loader):
+        for i, (inputs, target, _) in enumerate(val_loader):
+            data_time.update(time.time() - end)
+            if args.train_gpu:
+                inputs = inputs.cuda(non_blocking=True)
+                target = target.cuda(non_blocking=True)
+            out = model(inputs)
+            out, target = out.data, target.data
+            loss = criterion(out[:, -1:], target)
+            ss += criterion2(out[:, -1:], target)
 
-        metrics = Metrics(out[:, -1:], target[:, -1:])
-        metrics.cal_cls_metrics()
-        auc_meter.update(metrics.auc), iou_meter.update(metrics.iou)
-        loss_meter.update(loss.item(), inputs.size(0))
-        batch_time.update(time.time() - end)
-        end = time.time()
-        if (i + 1) % args.print_freq == 0:
-            print('Test: [{}/{}] '
-                  'Data {data_time.val:.3f} ({data_time.avg:.3f}) '
-                  'Batch {batch_time.val:.3f} ({batch_time.avg:.3f}) '
-                  'Loss {loss_meter.val:.4f} ({loss_meter.avg:.4f}) '
-                  'mIoU {iou_meter.avg:.3f}'.format(i + 1, len(val_loader),
-                                                    data_time=data_time, batch_time=batch_time,
-                                                    loss_meter=loss_meter,
-                                                    iou_meter=iou_meter))
+            metrics = Metrics()
+            metrics.cal_cls_metrics(out[:, -1:], target[:, -1:])
+            auc_meter.update(metrics.auc), iou_meter.update(metrics.iou)
+            loss_meter.update(loss.item(), inputs.size(0))
+            batch_time.update(time.time() - end)
+            end = time.time()
+            if (i + 1) % args.print_freq == 0:
+                print('Test: [{}/{}] '
+                      'Data {data_time.val:.3f} ({data_time.avg:.3f}) '
+                      'Batch {batch_time.val:.3f} ({batch_time.avg:.3f}) '
+                      'Loss {loss_meter.val:.4f} ({loss_meter.avg:.4f}) '
+                      'mIoU {iou_meter.avg:.3f}'.format(i + 1, len(val_loader),
+                                                        data_time=data_time, batch_time=batch_time,
+                                                        loss_meter=loss_meter,
+                                                        iou_meter=iou_meter))
     mIoU = iou_meter.avg
     print('Val result: mIoU/ssim {:.4f}/{:.4f}.'.format(mIoU, ss / num_img_tr))
     print('<<<<<<<<<<<<<<<<< End Evaluation <<<<<<<<<<<<<<<<<')
+    save_image(out[:, -1:].cpu(), saver.vis_dir + '/{}_val.png'.format(epoch))
+    # save_image(out[:, 0:3].data.cpu(), saver.vis_dir + '/{}_val_rgb.png'.format(epoch))
+    # save_image(aux.cpu(), saver.vis_dir + '/{}_val_aux.png'.format(epoch))
     return loss_meter.avg, mIoU, ss/num_img_tr
 
 
