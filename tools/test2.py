ebb2a4c5fe18c648f5c0691acf1108c36a6a6b6d
xia-xx-cv <49708396+xia-xx-cv@users.noreply.github.com>
2025-03-23 20:43:39 +0800

    test2.py
    Updated in March 2025 to move the creation of the "metric" instance out of the loop.

--- tools/test2.py
+++ tools/test2.py
@@ -1,19 +1,19 @@
-import os, time, logging, sys
-sys.path.append("../")
-# sys.path.append("../torchCode/MyNet/")
-import argparse, cv2
+import time, os
+import logging
+import argparse
 import torch, torch.nn
 import torch.nn.parallel
 import torch.backends.cudnn as cudnn
 import torch.utils.data as udata
 from torchvision.utils import save_image
-from datasets import data_reader_syn as data_reader_me
+from datasets import data_reader_syn as data_reader
 from util import config
 from util import transform as trans
-from util.util_smoke import AvgMeter, cal_cls_metrics, check_dir
+from util.util_smoke import AvgMeter, Metrics, check_dir
 from models.mynet_0205_ori import MyNet
 from torch.nn.functional import interpolate as it
 from util.cal_ssim import SSIM
+from tqdm import tqdm
 
 
 def get_logger():
@@ -39,9 +39,8 @@ def get_parser():
 def main():
     """
     pred_split = 'test_gt'(call test_gt function) or 'test_img'(call test_img function)
-    test: for test set with GT, and testing result will be saved in train_epoch_n.pth/xxx.txt
+    test_gt: for test set with GT, and testing result will be saved in train_epoch_n.pth/xxx.txt
     test_img: for all kinds of set, and the result images will be saved in SD01/02/03 file
-    :return:
     """
     global args, logger, device
     args = get_parser()
@@ -53,26 +52,27 @@ def main():
 
     # ---- args.dataset == 'smoke'-----------
     mean, std = [0.5, 0.5, 0.5], [0.3, 0.3, 0.3]
-    # value_scale = 255
-    # mean, std = [item * value_scale for item in mean], [item * value_scale for item in std]
-    test_trans = trans.Compose([trans.Resize(args.img_size),
-                                trans.ToTensor(),
-                                trans.Normalize(mean=mean, std=std),
-                                ])
-    test_data = data_reader_me.Smoke(split='vis', data_root=args.data_root,
-                                     data_list=args.test_list, transform=test_trans)
-    test_loader = udata.DataLoader(test_data, batch_size=1, shuffle=False, num_workers=args.workers, pin_memory=True)
+    test_trans = trans.Compose([
+        trans.Resize(),
+        trans.ToTensor(),
+        trans.Normalize(mean=mean, std=std)
+    ])
+    test_data = data_reader.Smoke(split='val', data_root=args.data_root,
+                                 data_list=args.test_list, transform=test_trans)
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=args.batch_size_val,
+                                             shuffle=False, num_workers=args.workers,
+                                             pin_memory=True, sampler=None)
+    device = 'cpu' if not args.train_gpu else 'cuda'
 
     model = MyNet(nclass=args.classes, backbone=args.backbone, pretrained=False,
-                  bn=torch.nn.BatchNorm2d)
-    device = 'cpu' if not args.train_gpu else 'cuda:{}'.format(args.train_gpu)
-    model = torch.nn.DataParallel(model)
-    if args.train_gpu:
-        model = model.cuda()
+                   bn=torch.nn.BatchNorm2d).eval().to(device)
+    model = torch.nn.DataParallel(model).cuda()
     cudnn.benchmark = True
+
     try:
         logger.info("=> loading checkpoint '{}'".format(args.model_path))
-        checkpoint = torch.load(args.model_path, map_location=torch.device(device))
+        # checkpoint = torch.load(args.resume, map_location=lambda storage, loc: storage.cuda())
+        checkpoint = torch.load(args.model_path)
         model.load_state_dict(checkpoint['state_dict'], strict=True)
         logger.info("=> loaded checkpoint '{}'".format(args.model_path))
     except Exception as err:
@@ -93,16 +93,16 @@ def main():
 def test_gt(test_loader, test_data, model, saveImage=False):
     # ------- with GT ----------
     data_time = AvgMeter()
-    miou, mse_meter, l = 0.0, 0.0, torch.nn.MSELoss()
-    ss, ssim = 0.0, SSIM(window_size=9)
+    mse_loss = torch.nn.MSELoss()
+    ssim = SSIM(window_size=9)
+    mse_meter, iou_meter, ss_meter = AvgMeter(), AvgMeter(), AvgMeter()
 
     # ------- preparing for saving results ----------
     logger.info('--------->>> Start Evaluation ------->>>')
-    model.eval()
     data_list = test_data.data_list
     dataset_name = args.test_list[0].split('/')[-1][0:4]
     # -----generating results file path
-    base_path = args.model_path.split('.')[0]
+    base_path = args.model_path.split('.pth')[0]
     save_path = os.path.join(base_path, dataset_name).replace('\\', '/')
     check_dir(save_path)
     # filename = args.model_path.split('/')[-1].split('.')[0] + '.txt'
@@ -110,44 +110,33 @@ def test_gt(test_loader, test_data, model, saveImage=False):
     # -----computing and writing metrics into text file
     f = open(filename, "w+")
     end = time.time()
+    metric = Metrics(th=0.2)
     with torch.no_grad():
-        for i, (image, target, _) in enumerate(test_loader):
+        for i, (image, target, _) in tqdm(enumerate(test_loader)):
             image = image.to(device)
             target = target.to(device)
             data_time.update(time.time() - end)
-            # out, _ = model(image)
             out = model(image)
             end = time.time()
-        # ------- calculating mse, iou, and ssim ----------
-            miou_i = cal_cls_metrics(out[:, -1:].data, targetout[:, -1:].data))
-            miou += miou_i
-            mse_i = l(out[:, -1:].data, target.data)
-            mse_meter += mse_i
-            ss_i = ssim(out[:, -1:].data, target.data)
-            ss += ss_i
+            # ------- calculating mse, iou, and ssim ----------
+            metric.cal_cls_metrics(out[:, -1:], target[:, -1:])
+            iou_i = metric.iou
+            mse_i = mse_loss(out[:, -1:].data, target.data)
+            ss_i = ssim(out[:,-1::].data.cpu(), target.data.cpu())
+            mse_meter.update(mse_i), iou_meter.update(iou_i), ss_meter.update(ss_i)
+
             # ------- writing every result into txt ----------
             image_path, _ = data_list[i]
             image_name = image_path.split('/')[-1].split('.')[0]
-            # ----- writing every visualization into image files ---
-            if saveImage:
-                save_image(out[:, -1],
-                           os.path.join(save_path, image_name+'_vis.png').replace('\\', '/'),
-                           normalize=False)
             out_single = 'result: name{} ---> mIoU={:.4f}%, MSE={:.4f}, mAcc={:.4f}%, ' \
-                         'time={:.4f}s'.format(image_name, miou_i, mse_i, ss_i, data_time.val)
+                         'time={:.4f}s'.format(image_name, iou_i * 100, mse_i, ss_i, data_time.val)
             f.write('\n')
             f.write(out_single)
-        # ------- printing results on console ----------
-            if ((i + 1) % 10 == 0) or (i + 1 == len(test_loader)):
-                logger.info('Test: [{}/{}] '
-                            'Data {data_time.val:.3f} ({data_time.avg:.3f}).'
-                            .format(i + 1, len(test_loader), data_time=data_time))
-
     # ------- mean results of one test set ----------
     logger.info('-------------- End Evaluation ---------------')
-    mIoU, mMse = miou/len(data_list), mseall/len(data_list)
+    miou, mmse, ss = iou_meter.avg, mse_meter.avg, ss_meter.avg
     out_info = 'result: ------> mIoU={:.2f}%, mMse={:.4f}, mSSIM={:.3f}, ' \
-               'time={:.4f}s'.format(mIoU*100, mse_meter/1000, ss/1000, mTime)
+               'time={:.4f}s'.format(miou * 100, mmse, ss, data_time.avg)
     logger.info(out_info)
 
     f.write('\n'), f.write(out_info), f.write('\n')
@@ -160,36 +149,31 @@ def test_img(test_loader, test_data, model):
     logger.info('---------- Start Prediction ----------')
     data_time = AvgMeter()
     # ------- preparing for saving results ----------
-    model.eval()
     data_list = test_data.data_list
     dataset_name = args.test_list[0].split('/')[-1][0:-4]
     # -----generating results file path
-    base_path = args.model_path.split('.')[0]
+    base_path = args.model_path.split('.pth')[0]
     save_path = os.path.join(base_path, dataset_name).replace('\\', '/')
     check_dir(save_path)
     filename = save_path + '.txt'
     f = open(filename, "w+")
     end = time.time()
     with torch.no_grad():
-        for i, (image, _, _) in enumerate(test_loader):
+        for i, (image, _, _) in tqdm(enumerate(test_loader)):
             data_time.update(time.time() - end)
             _, _, h, w = image.shape
-            image = it(image, ((h//2)*2-4, (w//2)*2-4), **up_kwargs)
-            out = model(image.to(device))
-            # out = out[:, -1:]
-        # ------- generating every file name ----------
+            # image = it(image, (int(h//2)*2, int(w//2)*2), **up_kwargs)
+            image = it(image, (512, 512), **up_kwargs)
+            out = model(image.cuda())
+            # ------- generating every file name ----------
             image_path, _ = data_list[i]
             image_name = image_path.split('/')[-1].split('.')[0]
             out_single = 'result: name{} ---> time={:.4f}s'.format(image_name, data_time.val)
             logger.info(out_single)
-        # ------- saving every estimated smoke image ----------
-            out2 = (out[-1].cpu().numpy().transpose(1, 2, 0))
-            # ---- ↓ ---black bg---
-            save_image(out[:, -1:],
-                       os.path.join(save_path, image_name+'_vis.png').replace('\\', '/'),
+            out = it(out[:,-1::].data.cpu(), (h, w), **up_kwargs)
+            save_image(out,
+                       os.path.join(save_path, image_name + '_vis.png').replace('\\', '/'),
                        normalize=False)
-
-        
 
     logger.info('---------- End Prediction ------------')
     single_time, all_time = data_time.avg, data_time.sum
@@ -199,7 +183,6 @@ def test_img(test_loader, test_data, model):
     f.write(out_info)
     f.write('\n')
     f.close()
-
 
 if __name__ == '__main__':
     main()
